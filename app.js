8bbdfbec5dcd3b7b39daa4440c3715e104138576
Nick McKendry <nickmckendry@gmail.com>
2017-02-28 13:50:30 -0700

    addded styles

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-$(document).ready(function(){
-  console.log('ready')
-  $.get('http://www.omdbapi.com/?t=rambo', function(data){
-    console.log(data)
+$(document).ready(function() {
+  $('button').click(function(){
+    getMovie()
+
   })
-})
+  })
+    console.log('ready')
+    $.get('http://www.omdbapi.com/?t=rambo', function(data) {
+        console.log(data)
+    })
+
+    function getMovie(){
+      var url = "http://www.omdbapi.com/?t=rambo"
+      url += $('input').val()
+      $.get(url, function(data){
+        console.log(url);
+      })
+    }
+    function updatePage(title) {
+      $('body').append(title)
+    }
+  
